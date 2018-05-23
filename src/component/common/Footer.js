d6edf1dee6a0dc24a5bcb68d2f5b8f7b98b76891
jieunjieun <kyn07077@gmail.com>
2018-05-23 19:15:35 +0900

    install react-router-dom

---
+++ src/component/common/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.scss';
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default Footer;
