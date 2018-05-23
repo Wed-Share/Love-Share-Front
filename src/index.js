2878f4d91005b404e84d6683506ac93179967aab
seoyunho <yunho101775@gmail.com>
2018-05-23 17:01:44 +0900

    [DELETE] 필요없는 파일 제거

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Landing from './container/Landing';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Landing />, document.getElementById('root'));
-registerServiceWorker();
